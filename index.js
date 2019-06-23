3db931dd4458e4347e5b292aa7645042f160a450
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-06-23 23:10:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function heyJude(){
+  
+  const line =  "Na na na na na na na, na na na na, hey Jude.";
+  return line.repeat(16);
+}
+
+function prohibitedLanguage(username) {
+  
+  let myRegExp = (\candycorn\,\brusselssprouts\);
+  
+  return myRegExp.test(username);
+ 
+ 
+}
